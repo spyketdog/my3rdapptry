406ec5d6fc43e3761f6186bb4d129939ace790bf
spyketdog <fpruis@gmail.com>
2015-07-01 10:51:30 +0200

    Installed pins scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150623123651) do
+ActiveRecord::Schema.define(version: 20150701082329) do
+
+  create_table "pins", force: :cascade do |t|
+    t.string   "description", limit: 255
+    t.string   "string",      limit: 255
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
