be31d95c5b7fc9b004f0eaa700085377341f234b
spyketdog <fpruis@gmail.com>
2015-07-10 13:59:30 +0200

    edited development rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,14 +41,5 @@ Rails.application.configure do
 
   # equired for Heroku
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
-  #For testing amazon S3 uploads locally
-  config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  }
-}
+  
 end
